199ca57e5c0a9573906642aa118cab1a62d8662d
pritesh511 <patelpritesh313@gmail.com>
2023-12-26 18:38:40 +0530

    Integrate delete API

--- todo-fe/index.js
+++ todo-fe/index.js
@@ -1,6 +1,7 @@
 const inputField = document.getElementById("titleInput");
 const titleError = document.getElementById("titleError");
 const addTodoBtn = document.getElementById("addTodoBtn");
+const deleteIcon = document.getElementById("deleteIcon");
 const listWrap = document.getElementById("listWrap");
 const API_URL = "http://localhost:3000";
 
@@ -11,9 +12,25 @@ inputField.addEventListener("change", (event) => {
 window.onload = async function () {
   const allList = await getAllTodos();
   allList.data.forEach((item) => {
-    const li = document.createElement("ol");
+    const li = document.createElement("li");
+    const img = document.createElement("img");
+    const span = document.createElement("span");
+
     li.className = "todo-item";
-    li.innerHTML = item.title;
+    li.setAttribute("id", "todoItem");
+    span.innerHTML = item.title;
+
+    img.src =
+      "https://img.freepik.com/premium-vector/red-cross-mark-icon-negative-choice-symbol-sign-app-button_744955-339.jpg?w=740";
+    img.className = "cross-icon";
+    img.addEventListener("click", function () {
+      const deleteTodoItem = deleteTodo(item._id);
+      console.log(deleteTodoItem);
+      location.reload();
+    });
+
+    li.appendChild(span);
+    li.appendChild(img);
     listWrap.appendChild(li);
   });
 };
@@ -60,5 +77,21 @@ async function getAllTodos() {
     return requestRes;
   } catch (err) {
     console.log("err", err);
+  }
+}
+
+async function deleteTodo(id) {
+  console.log(id);
+  try {
+    const deleteTodo = await fetch(API_URL + `/todo?id=${id}`, {
+      method: "delete",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const response = deleteTodo.json();
+    return response;
+  } catch (error) {
+    console.log(error);
   }
 }
