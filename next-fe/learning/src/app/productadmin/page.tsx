e7255c25da81d1c5cd5f54e97d33161913e019a5
pritesh511 <patelpritesh313@gmail.com>
2023-12-27 16:36:09 +0530

    create product create form

--- next-fe/learning/src/app/productadmin/page.tsx
+++ next-fe/learning/src/app/productadmin/page.tsx
@@ -1,7 +1,94 @@
 import React from "react";
 
 const BlogAdmin = () => {
-  return <div>BlogAdmin</div>;
+  return (
+    <div className="bg-gray-900 w-full h-screen flex items-center justify-center">
+      <div className="max-w-[400px] w-full px-4">
+        <form className="w-full mx-auto">
+          <div className="mb-5">
+            <label
+              htmlFor="text"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Enter product name
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter product name"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="large-input"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Enter product description
+            </label>
+            <input
+              type="text"
+              id="large-input"
+              placeholder="Enter product description"
+              className="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="number"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Enter product price
+            </label>
+            <input
+              type="number"
+              id="price"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter product price"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="number"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Enter product discount
+            </label>
+            <input
+              type="number"
+              id="discount"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter product discount"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="text"
+              className="block mb-2 text-sm font-medium text-white"
+            >
+              Enter product image URL
+            </label>
+            <input
+              type="text"
+              id="image"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Enter product image URL"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default BlogAdmin;
