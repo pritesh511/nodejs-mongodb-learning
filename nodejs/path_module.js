785b672fbdc526d07871f01aae62c476d86e58bb
pritesh511 <patelpritesh313@gmail.com>
2023-12-28 15:37:42 +0530

    path module learning

---
+++ nodejs/path_module.js
@@ -0,0 +1,9 @@
+const path = require("path");
+
+console.log(path.dirname("nodejs/path_module.js"));
+
+console.log(path.basename("nodejs/path_module.js"));
+
+console.log(path.extname("nodejs/path_module.js"));
+
+console.log(path.parse("nodejs/path_module.js"));
