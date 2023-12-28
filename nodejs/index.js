b8c74e72494f9dcbab6df59ff0d616548c07ef6a
pritesh511 <patelpritesh313@gmail.com>
2023-12-28 18:11:36 +0530

    create routing and render code

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,8 +1,17 @@
-// Dummy data
 const http = require("http");
 
-const server = http.createServer((_req, res) => {
-  res.end("This is server");
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    res.end("This is home page loaded");
+  } else if (req.url == "/contact") {
+    res.end("<h1>Hello Home page</h1>");
+  } else if (req.url == "/about") {
+    res.end("This is about page page loaded");
+  } else {
+    res.end("404 Error Page not found");
+  }
 });
 
-server.listen(8000);
+server.listen(8000, () => {
+  console.log("Your server is running on port 8000");
+});
