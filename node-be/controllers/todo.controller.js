198a039aa735a9b069ecec5514e89133bab8c043
pritesh511 <patelpritesh313@gmail.com>
2024-01-01 11:53:47 +0530

    Added successsmessage in create todo API

--- node-be/controllers/todo.controller.js
+++ node-be/controllers/todo.controller.js
@@ -3,7 +3,7 @@ const todoService = require("../services/todo.service");
 exports.todoCreate = async (req, res) => {
   try {
     const todo = await todoService.createTodo(req.body);
-    res.status(200).json(todo);
+    res.status(200).json({ todo, message: "Todo added successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
