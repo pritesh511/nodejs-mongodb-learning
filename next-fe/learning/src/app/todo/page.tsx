43a876cef441eeb3908877a8aac6175c01187960
pritesh511 <patelpritesh313@gmail.com>
2024-01-01 11:54:56 +0530

    Create toster component and implement

--- next-fe/learning/src/app/todo/page.tsx
+++ next-fe/learning/src/app/todo/page.tsx
@@ -2,11 +2,14 @@
 import React, { useEffect, useState } from "react";
 import TodoItem from "../components/TodoItem";
 import { API_URL, GET, POST, todoItem } from "@/constant";
+import Toster from "../components/Toster";
 
 const Todo = () => {
   const [todoTitle, setTodoTitle] = useState("");
   const [todoTitleError, setTodoTitleError] = useState(false);
   const [todoList, setTodoList] = useState([]);
+  const [isTosterOpen, setIsTosterOpen] = useState(false);
+  const [todoMessage, setTodoMessage] = useState("");
 
   useEffect(() => {
     getAllTodos();
@@ -39,7 +42,10 @@ const Todo = () => {
         body: JSON.stringify(body),
       });
       const createTodoResp = await createTodo.json();
-      console.log(createTodoResp);
+      if (createTodoResp) {
+        setIsTosterOpen(true);
+        setTodoMessage(createTodoResp.message);
+      }
       setTodoTitle("");
       getAllTodos();
     } else {
@@ -53,44 +59,59 @@ const Todo = () => {
     }
   }
 
+  function handleCloseToster() {
+    setIsTosterOpen(false);
+  }
+
   return (
-    <div className="w-full h-screen bg-gradient-to-r from-sky-500 to-indigo-500">
-      <div className="max-w-[1000px] mx-auto px-4 h-full">
-        <div className="pt-6">
-          <form
-            onKeyDown={onKeyDownHandler}
-            onSubmit={(e) => createTodo(e)}
-            className="flex gap-3 "
-          >
-            <input
-              onChange={(e) => {
-                setTodoTitle(e.target.value);
-                setTodoTitleError(false);
-              }}
-              value={todoTitle}
-              placeholder="Please Enter Todo Title"
-              className="p-3 rounded-lg w-full border-2 border-pink-500/100 focus:outline-none"
-            />
-            <button
-              type="submit"
-              className=" w-20 rounded-lg text-gray-50 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ..."
+    <>
+      <div className="w-full h-screen bg-gradient-to-r from-sky-500 to-indigo-500">
+        <div className="max-w-[1000px] mx-auto px-4 h-full">
+          <div className="pt-6">
+            <form
+              onKeyDown={onKeyDownHandler}
+              onSubmit={(e) => createTodo(e)}
+              className="flex gap-3 "
             >
-              Add
-            </button>
-          </form>
+              <input
+                onChange={(e) => {
+                  setTodoTitle(e.target.value);
+                  setTodoTitleError(false);
+                }}
+                value={todoTitle}
+                placeholder="Please Enter Todo Title"
+                className="p-3 rounded-lg w-full border-2 border-pink-500/100 focus:outline-none"
+              />
+              <button
+                type="submit"
+                className=" w-20 rounded-lg text-gray-50 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 ..."
+              >
+                Add
+              </button>
+            </form>
+          </div>
+          {todoTitleError && (
+            <span className="text-red-900">Please Enter todo title</span>
+          )}
+          <ul className="mt-4 h-[calc(100vh_-_97px)] overflow-auto">
+            {todoList.map((item: todoItem) => {
+              return (
+                <TodoItem
+                  key={item._id}
+                  todo={item}
+                  getAllTodos={getAllTodos}
+                />
+              );
+            })}
+          </ul>
         </div>
-        {todoTitleError && (
-          <span className="text-red-900">Please Enter todo title</span>
-        )}
-        <ul className="mt-4 h-[calc(100vh_-_97px)] overflow-auto">
-          {todoList.map((item: todoItem) => {
-            return (
-              <TodoItem key={item._id} todo={item} getAllTodos={getAllTodos} />
-            );
-          })}
-        </ul>
       </div>
-    </div>
+      <Toster
+        open={isTosterOpen}
+        onClose={handleCloseToster}
+        message={todoMessage}
+      />
+    </>
   );
 };
 
